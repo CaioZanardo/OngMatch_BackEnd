4ff68e0f9c8421c988c606bf6d5c70e5e4427892
Caio Zanardo <55753694+CaioZanardo@users.noreply.github.com>
2023-06-05 23:03:53 -0300

    Update index.php

--- src/_helpers/index.php
+++ src/_helpers/index.php
@@ -18,24 +18,31 @@ function siteHeader($title) {
                 
                 <title>{$title}</title>
             </head>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="/">Ong Match BACKEND</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link" href="{$BASE}user">User</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{$BASE}infoUser">Cadastro</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+             <nav class="navbar navbar-expand-lg bg-success sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                <img src="./assets/images/ONF.png" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                        <a class="nav-link" href="{$BASE}">Home</a>
+                    </li>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{$BASE}user">User</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{$BASE}infoUser">Cadastro</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
         
     HTML;
     }

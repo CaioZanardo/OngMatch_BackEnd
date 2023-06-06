b3e7f6a62293cc0846cf87a0d2ddd6cf4ea75a0b
CaioZanardo <caio_zanardo@hotmail.com>
2023-06-05 21:26:33 -0300

    Incluindo GS

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php 
+	require "_helpers/index.php"; 
+	echo siteHeader("InfoUser"); //chama a função para retornar o Header
+?>
+    
+    <section class="container mt-5 mb-5">
+        <div class="row mb-3">
+            <div class="col">
+                <h1>Sistema de Cadastro de Usuarios Ong Match</h1> 
+            </div>
+        </div>
+    </section> 
+</body>
+
+</html>
